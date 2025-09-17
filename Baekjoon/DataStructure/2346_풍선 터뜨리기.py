491ed146b8a6e7cf5abdfd4e22988bc0cacb5982
yeonjy73 <yeon703@naver.com>
2025-09-17 17:13:11 +0900

    docs: add 2346

--- Baekjoon/DataStructure/2346_풍선 터뜨리기.py
+++ Baekjoon/DataStructure/2346_풍선 터뜨리기.py
@@ -4,14 +4,13 @@ output = sys.stdout
 from collections import deque
 
 n = int(input().rstrip())
-list = list()
 queue = deque((i, word) for i, word in enumerate(map(int, input().rstrip().split()), start=1))
 
 for _ in range(n):
     popnum = queue.popleft()
     output.write(str(popnum[0]) + " ")
     if popnum[1] > 0:
-        queue.rotate(popnum[1] - 1)
+        queue.rotate(len(queue) + 1 - popnum[1])
     else:
         queue.rotate(popnum[1] * -1)
 
