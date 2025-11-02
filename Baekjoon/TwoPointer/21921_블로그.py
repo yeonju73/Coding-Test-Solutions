21b03f8570e4ac3002029d7f29dbe618a8fac3be
yeonjy73 <yeon703@naver.com>
2025-11-02 17:44:03 +0900

    docs: 21921 슬라이딩 윈도우

--- Baekjoon/TwoPointer/21921_블로그.py
+++ Baekjoon/TwoPointer/21921_블로그.py
@@ -3,21 +3,24 @@ input = sys.stdin.readline
 
 n, x = map(int, input().rstrip().split())
 input_list = list(map(int, input().rstrip().split()))
-sum_list = [0] * (n - x + 1)
 
-for i in range(n):
-    for j in range(x):
-        index = i - j
-        if 0 <= index < len(sum_list):
-            sum_list[index] += input_list[i]
-
-max_value = max(sum_list)
-if max_value == 0:
+if max(input_list) == 0:
     print("SAD")
-else:
-    count = 0
-    for i in sum_list:
-        if i == max_value:
+else: 
+    # 슬라이딩 윈도우: 이전 구간의 합에서 맨 왼쪽 값은 빼고, 새로 들어온 맨 오른쪽 값은 더힘
+    window_sum = sum(input_list[0:x])
+    max_value = window_sum
+    count = 1
+    
+    for i in range(x, n):
+        window_sum = window_sum + input_list[i] - input_list[i - x]
+        
+        # 더 큰 값이 나오면 교체하고 count 초기화
+        if max_value < window_sum:
+            max_value = window_sum
+            count = 1
+        elif max_value == window_sum:
             count += 1
+    
     print(max_value)
     print(count)
