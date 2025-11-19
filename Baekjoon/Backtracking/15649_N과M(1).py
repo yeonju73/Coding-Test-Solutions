821fae8b2b122bb9dcd4ac9aadd7018a8d1c6d9e
yeonjy73 <yeon703@naver.com>
2025-11-19 15:50:41 +0900

    refactor: 15649

--- Baekjoon/Backtracking/15649_N과M(1).py
+++ Baekjoon/Backtracking/15649_N과M(1).py
@@ -1,29 +1,41 @@
 import sys
 input = sys.stdin.readline
 
-def backtracking(current_list:list):
-    if len(current_list) == M:
-        result_list.append(current_list.copy())
-        return
-    for i in range(1, N+1):
-        if visited[i] :
-            continue
-        current_list.append(i)
-        visited[i] = True
-        backtracking(current_list)
-        # 복구
-        visited[i] = False
-        current_list.pop()
-
 N, M = map(int, input().rstrip().split())
-result_list = list()
-visited = {i:False for i in range(1, N+1)}
-current_list = list()
 
-backtracking(current_list)
+def solve():
+    numbers = [i for i in range(1, N+1)]
+    result_list = backtracking_permutations(numbers, M)
+    for result in result_list:
+        print(*result) # 리스트 안의 값을 공백으로 자동 구분해서 출력
+
+def backtracking_permutations(nums, size):
+    result = []
+    visited = [False] * len(nums)
+    path = []
     
+    def dfs():
+        # 종료 조건
+        if len(path) == size:
+            result.append(path.copy())
+            return
+        
+        # 모든 후보 탐색
+        for i in range(len(nums)):
+            if visited[i]:
+                continue
+            # 선택 (go down)
+            visited[i] = True
+            path.append(i+1)
+            
+            # 재귀호출
+            dfs()
+            
+            # 되돌리기
+            visited[i] = False
+            path.pop()
+    
+    dfs()
+    return result
 
-for result in result_list:
-    for r in result:
-        print(r, end=' ')
-    print()
+solve()
