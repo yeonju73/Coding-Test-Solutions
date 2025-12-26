79ee45d088f2fb30579e0c042c618bc3ae2a2308
yeonjy73 <yeon703@naver.com>
2025-12-26 17:18:05 +0900

    refactor: add 14940

--- Baekjoon/GraphTraversal/14940_쉬운 최단거리.py
+++ Baekjoon/GraphTraversal/14940_쉬운 최단거리.py
@@ -9,42 +9,38 @@ n, m = map(int, input().rstrip().split())
 direc = ((0, -1), (0, 1), (-1, 0), (1, 0))
 
 my_list = list()
+# 거리 저장을 위한 배열 선언
+dist = [[-1]*m for _ in range(n)]
 start: tuple
 
 for i in range(n):
-    input_list = list(map(int, input().rstrip().split()))
-    if 2 in input_list:
-        start = (i, input_list.index(2))
-    my_list.append(input_list)
+    row = list(map(int, input().rstrip().split()))
+    for j in range(m):
+        if row[j] == 2:
+            start = (i, j)
+    my_list.append(row)
 
 
 # bfs
-queue = deque(list())
-visited = set()
-queue.append((start[0], start[1], 0))
+queue = deque([start])
+dist[start[0]][start[1]] = 0
 
 while queue:
     current = queue.popleft()
-    current_point = (current[0], current[1])
-    
-    if current_point not in visited:
-        visited.add(current_point)
+    x, y = current[0], current[1]
         
-        # 처음 방문일 때만 값 업데이트
-        value = current[2]
-        my_list[current_point[0]][current_point[1]] = value
-        
-        for d in direc:
-            dx = current_point[0] + d[0]
-            dy = current_point[1] + d[1]
-            
-            if 0 <= dx < n and 0 <= dy < m and my_list[dx][dy] != 0:
-                queue.append((dx, dy, value+1))
+    for dx, dy in direc:
+        nx, ny = x + dx, y + dy
+        # dist[nx][ny] == -1 구역을 방문 안 한 구역이라 생각
+        if 0 <= nx < n and 0 <= ny < m and my_list[nx][ny] != 0 and dist[nx][ny] == -1:
+            # 큐에 넣을 때 방문하고 값 업데이트
+            dist[nx][ny] = dist[x][y] + 1
+            queue.append((nx, ny))
                 
-for line in range(n):
-    for i in range(m):
-        if my_list[line][i] == 1 and (line, i) not in visited:
-            print(-1, end=' ')
+for i in range(n):
+    for j in range(m):
+        if my_list[i][j] == 0:
+            print(0, end=' ')
         else:
-            print(my_list[line][i], end=' ')
+            print(dist[i][j], end=' ')
     print()
