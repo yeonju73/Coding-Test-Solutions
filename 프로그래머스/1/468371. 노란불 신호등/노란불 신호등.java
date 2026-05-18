aaf9cd5d2b1bab7118e5878789832ad6f7e53c9b
박연주 <108404405+yeonju73@users.noreply.github.com>
2026-05-18 15:38:37 +0900

    [level 1] Title: 노란불 신호등, Time: 18.38 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] signals) {
+        
+        // 신호등이 몇개인지
+        int signal_size = signals.length;
+        ArrayList<ArrayList<Character>> sinal_array = new ArrayList<>(signal_size);
+        
+        for (int i = 0; i < signal_size; i++) {
+            ArrayList<Character> temp = new ArrayList<>();
+            
+            for (int g = 0; g < signals[i][0]; g++) {
+                temp.add('G');
+            }
+            for (int y = 0; y < signals[i][1]; y++) {
+                temp.add('Y');
+            }
+            for (int r = 0; r < signals[i][2]; r++) {
+                temp.add('R');
+            }
+            sinal_array.add(temp);
+        }
+        
+        // for (ArrayList<Character> c: sinal_array) {
+        //     for (Character cc : c) {
+        //         System.out.print(cc);
+        //     }
+        //     System.out.println();
+        // }
+        
+        boolean flag = true;
+        for (int time = 0; time < 2000000; time++) { 
+            flag = true;
+            
+            for (int s = 0; s < signal_size; s++) {
+                ArrayList<Character> curSignal = sinal_array.get(s);
+                int curSize = curSignal.size();
+                if (curSignal.get(time % curSize) != 'Y'){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) return time + 1;
+        }
+        return -1;
+    }
+}
