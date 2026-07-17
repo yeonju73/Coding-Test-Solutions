36672fa89f6722a6440b1e157d2d0ae04c1e585e
박연주 <108404405+yeonju73@users.noreply.github.com>
2026-07-18 01:50:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.98 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,42 @@
 import java.util.*;
 
 class Solution {
-    int answer = -1;
-    int[] dx = {0, 0, 1, -1};
-    int[] dy = {1, -1, 0, 0};
-    
     public int solution(int[][] maps) {
+        int answer = 0;
+        
         int n = maps.length;
         int m = maps[0].length;
         
-        bfs(0, 0, maps, n, m);
-
-        if (answer == Integer.MAX_VALUE) {
-            answer = -1;
-        }
-        return answer;
-    }
-    
-    public void bfs(int x, int y, int[][] maps, int n, int m) {
+        int[][] visited = new int[n][m];
+        
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         Queue<int[]> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][m];
         
-        queue.add(new int[]{x, y, 0});                                                                         
-        visited[x][y] = true;
+        queue.offer(new int[]{0, 0});
+        visited[0][0] = 1;
         
-        while (!queue.isEmpty()){
-            int[] node = queue.remove();
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
             
-            if (node[0] == n-1 && node[1] == m-1) {
-                answer = node[2]+1;
-                return;
+            int i = current[0];
+            int j = current[1];
+        
+            if (i == n-1 && j == m-1) {
+                break;
             }
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = node[0] + dx[i];
-                int ny = node[1] + dy[i];
+            for (int d = 0; d < 4; d++) {
+                int nextX = i + dx[d];
+                int nextY = j + dy[d];
 
-                if ((0 <= nx && nx < n) && (0 <= ny && ny < m)) {
-                    if (maps[nx][ny] == 1 && !visited[nx][ny]) {
-                        queue.add(new int[]{nx, ny, node[2]+1});
-                        visited[nx][ny] = true;
-                    }
+                if(nextX >= 0 && nextX < n && nextY >= 0 && nextY < m && (maps[nextX][nextY] == 1) && (visited[nextX][nextY] == 0)){
+                    queue.offer(new int[]{nextX, nextY});
+                    visited[nextX][nextY] = visited[i][j]+1;
                 }
             }
         }
+        
+        if (visited[n-1][m-1] == 0) return -1;
+        return visited[n-1][m-1];
     }
 }
