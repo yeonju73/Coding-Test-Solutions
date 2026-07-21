6f22bfbcc2e874da24c584b80be438573c72e23b
박연주 <108404405+yeonju73@users.noreply.github.com>
2026-07-21 17:49:21 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 4.53 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -7,7 +7,6 @@ class Solution {
         
         int park_h = park[0].length;
         int park_r = park.length;
-        System.out.println("park_h = " + park_h + ", park_r = " + park_r);
     
         for (int i = mats.length-1; i >= 0; i--) {
             int size = mats[i];
@@ -16,11 +15,12 @@ class Solution {
                 for (int k = 0; k < park_h; k++) {
                     // 빈 칸이고 크기 가능이라면
                     if (park[j][k].equals("-1") && (j+size <= park_r) && (k+size <= park_h)) {
-                        System.out.println("j = " + j + ", k = " + k);
                         
                         boolean flag = true;
+                        // 돗자리 크기 내에서 불가능 한 좌석 없는지 확인
                         for (int n = 0; n < size; n++) {
                             for (int m = 0; m < size; m++) {
+                                // 비어있는 자리가 아니라면 flag 변경 후 break
                                 if (!park[j+n][k+m].equals("-1")){
                                     flag = false;
                                     break;
@@ -28,6 +28,7 @@ class Solution {
                             }
                             if (!flag) break;
                         }
+                        // 모든 땅 순회했을 때다 가능이었다면
                         if (flag) {
                             return size;
                         }
