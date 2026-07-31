0c9402a489224127cf99ec49dd2e4d00657e75b9
박연주 <108404405+yeonju73@users.noreply.github.com>
2026-07-31 14:49:00 +0900

    [level 3] Title: 합승 택시 요금, Time: 19.55 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Solution {
     class Node implements Comparable<Node> {
-        int index;
+        int number;
         int distance;
         
-        public Node(int index, int distance){
-            this.index = index;
+        public Node(int number, int distance) {
+            this.number = number;
             this.distance = distance;
         }
         
@@ -17,75 +17,81 @@ class Solution {
     }
     
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int edge = fares.length;
+        int answer = Integer.MAX_VALUE;
         
-        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-        
-        // 초기화
-        for(int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        // 양방향 그래프
-        for(int[] f: fares){
-            graph.get(f[0]).add(new Node(f[1], f[2]));
-            graph.get(f[1]).add(new Node(f[0], f[2]));
-        }
-        
-        // 최단 거리 테이블
-        int[] distS = new int[n + 1];
-        int[] distA = new int[n + 1];
-        int[] distB = new int[n + 1];
+        // 최단 거리 배열
+        int[] distS = new int[n]; 
+        int[] distA = new int[n];
+        int[] distB = new int[n];
         
         Arrays.fill(distS, Integer.MAX_VALUE);
         Arrays.fill(distA, Integer.MAX_VALUE);
         Arrays.fill(distB, Integer.MAX_VALUE);
         
-        dijkstra(s, graph, distS);
-        dijkstra(a, graph, distA);
-        dijkstra(b, graph, distB);
+        // node별 연결된 간선 저장
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
         
-        // 모든 정점 X를 합승 해제 지점으로 가정하고 최소 비용 계산
-        int answer = Integer.MAX_VALUE;
-        for(int x = 1; x <= n; x++) {
-            if(distS[x] != Integer.MAX_VALUE && distA[x] != Integer.MAX_VALUE && distB[x] != Integer.MAX_VALUE) {
-                int cost = distS[x] + distA[x] + distB[x];
-                answer = Math.min(answer, cost);
+        for(int i = 0; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        // 그래프 초기화
+        for(int[] f : fares){
+            graph.get(f[0]-1).add(new Node(f[1]-1, f[2]));
+            graph.get(f[1]-1).add(new Node(f[0]-1, f[2]));
+        }
+        
+        dijkstra(s-1, graph, distS);
+        dijkstra(a-1, graph, distA);
+        dijkstra(b-1, graph, distB);
+        
+        // 모든 노드를 합승 중단 지점 X로 하여
+        // start->X  + X->A + X->B 값이 최소인거
+        for(int i = 0; i < n; i++) {
+            if((distS[i] != Integer.MAX_VALUE) && (distA[i] != Integer.MAX_VALUE)&& (distB[i] != Integer.MAX_VALUE)){
+                int cost = distS[i] + distA[i] + distB[i];
+                answer = Math.min(cost, answer);
             }
         }
         
         return answer;
     }
     
-    public void dijkstra(int start, ArrayList<ArrayList<Node>> graph, int[] d) {
-        // 거리가 짧은 순으로 보기 위해 우선순위 큐에 삽입
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    public void dijkstra(int start, ArrayList<ArrayList<Node>> graph, int[] dist) {
+        Queue<Node> pq = new PriorityQueue<>();
         
+        // 시작 노드 insert
         pq.offer(new Node(start, 0));
-        d[start] = 0;
+        // 시작지점까지의 최단 거리는 0
+        dist[start] = 0;
         
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()) {
+            
             Node node = pq.poll();
-            int dist = node.distance;
-            int now = node.index;
+            int number = node.number;
+            int distance  = node.distance;
             
-            // 현재 노드가 이미 처리된 적이 있는 노드면 무시
-            if (d[now] < dist) continue;
+            if(dist[number] < distance) continue;
             
-            // 인접한 다른 노드 확인
-            for(int i = 0; i < graph.get(now).size(); i++) {
-                int cost = d[now] + graph.get(now).get(i).distance;
-                
-                // 현재 노드 통해서 인접노드로 가는 거리 < 인접 노드로 직행
-                if (cost < d[graph.get(now).get(i).index]) {
-                    // 인접 노드로 가는 최단 거리 갱신
-                    d[graph.get(now).get(i).index] = cost;
-                    pq.offer(new Node(graph.get(now).get(i).index, cost));
-                    
+            // node 연결되어있는 거를 순회
+            // 걔를 거쳐가는게 더 이득인지 판단!!
+            for(Node n : graph.get(number)){
+                // dist[number]: 현재 노드까지 가는 최단 거리
+                // n.distance: 현재 노드에서 인접 노드까지 가는 거리
+                int cost = dist[number] + n.distance;
+                // 인접 노드로 가는 최단 거리 갱신
+                // 현재 노드를 거쳐서 가는 것이 더 최단이라면
+                // dist[n.number]: 지금 저장된 n번 노드로 가는 최단거리
+                if (cost < dist[n.number]){
+                    dist[n.number] = cost;
+                    // 값이 변경된게 있으면 큐에 넣어서 인접된 노드까지 최단 거리 변경이 있는지 확인
+                    pq.offer(new Node(n.number, dist[n.number]));
                 }
             }
             
         }
+        
+        
         
     }
 }
