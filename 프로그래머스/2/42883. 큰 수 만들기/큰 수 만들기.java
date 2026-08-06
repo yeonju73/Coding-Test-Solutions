27ad2c23956558b057ae19b50c056b72a65c417a
박연주 <108404405+yeonju73@users.noreply.github.com>
2026-08-06 14:32:04 +0900

    [level 2] Title: 큰 수 만들기, Time: 32.64 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,12 +1,13 @@
 import java.util.*;
-// 숫자는 왼쪽의 값이 클 수록 큼
-// 내 바로 뒤에 있는 숫자보다 내가 작으면 나를 지움
-// stack으로 처음부터 넣는다
-// 뒤에 넣을 숫자가 stack top 보다 크면 stack pop
-// pop은 총 k 번 하기
-// 9876 같이 역으로 정렬되어 있는 숫자는 이 방식이 안되니까
-// 지운 값이 없을 경우 뒤에서 부터 k 개 잘라줘야 한다.
-
+/*
+    숫자는 왼쪽의 값이 클 수록 큼
+    내 바로 뒤에 있는 숫자보다 내가 작으면 나를 지움
+    stack으로 처음부터 넣는다
+    뒤에 넣을 숫자가 stack top 보다 크면 stack pop
+    pop은 총 k 번 하기
+    9876 같이 역으로 정렬되어 있는 숫자는 이 방식이 안되니까
+    지운 값이 없을 경우 뒤에서 부터 k 개 잘라줘야 한다.
+*/
 class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
